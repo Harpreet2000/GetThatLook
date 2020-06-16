97f414b053c4becf1124366fc883585c125856c7
Harpreet Kaur <200425194@student.georgianc.on.ca>
2020-06-16 16:09:35 -0400

    Changed theme throgh Bootswatch and added a Categories Page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace GetThatLook.Controllers
         {
             return View();
         }
+        public IActionResult Categories()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
