cabb4cfa9a7e2038017dc6270a67d0249500586a
Harpreet Kaur <200425194@student.georgianc.on.ca>
2020-06-16 15:53:49 -0400

    Created an About Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace GetThatLook.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
